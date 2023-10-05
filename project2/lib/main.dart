7adc24d9bb487315b34207ec7e12bd6dbd4be761
muhammadfajrinlubis <muhammadfajrinlubis3@gmail.com>
2023-10-05 15:02:12 +0700

    .

---
+++ project2/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("fajrinapp"),
+          ),
+          body: Center(
+            child: Text("hallo"),
+          ),
+        ));
+  }
+}
