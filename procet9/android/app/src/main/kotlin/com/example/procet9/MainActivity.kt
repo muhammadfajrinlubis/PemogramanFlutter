e843649f5b67850487c45e47d1e60aa8f87f0368
muhammadfajrinlubis <muhammadfajrinlubis3@gmail.com>
2023-11-30 22:54:13 +0700

    baru

--- procet9/android/app/src/main/kotlin/com/example/procet9/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.procet9
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- project12/android/app/src/main/kotlin/com/example/project12/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.project12
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
